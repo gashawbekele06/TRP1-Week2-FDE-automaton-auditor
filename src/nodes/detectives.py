625e8658b42d82a1415fffef0854e2ed5d75d9bb
gashawbekele06 <gashawbekelek@gmail.com>
2026-02-25 11:25:36 +0300

    feat: Add `Dict` and `List` imports for type hinting.

--- src/nodes/detectives.py
+++ src/nodes/detectives.py
@@ -1,6 +1,8 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
+from typing import Dict, List
+
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 
